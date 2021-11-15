db351986e4808193f988ddd37cf12d200c83108b
st156832 <st156832@stud.uni-stuttgart.de>
2021-11-15 10:49:55 +0100

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,5 @@
 open module exercise {
     requires de.hamstersimulator.objectsfirst.main;
-    exports de.unistuttgart.iste.sqa.pse.sheet05.presence;
+    exports de.unistuttgart.iste.sqa.pse.sheet05.presence.sort;
     exports de.unistuttgart.iste.sqa.pse.sheet05.presence.controlflowexercise;
 }
