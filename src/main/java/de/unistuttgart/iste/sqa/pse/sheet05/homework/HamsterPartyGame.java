0f7b759d7d1e26775903b3a233e04b9617244f52
yungflori <floris.schneyinck@gmail.com>
2023-11-23 21:07:15 +0100

    Update HamsterPartyGame.java

--- src/main/java/de/unistuttgart/iste/sqa/pse/sheet05/homework/HamsterPartyGame.java
+++ src/main/java/de/unistuttgart/iste/sqa/pse/sheet05/homework/HamsterPartyGame.java
@@ -1,14 +1,56 @@
 package de.unistuttgart.iste.sqa.pse.sheet05.homework;
 
+import de.hamstersimulator.objectsfirst.datatypes.Direction;
+import de.hamstersimulator.objectsfirst.datatypes.Location;
+import de.hamstersimulator.objectsfirst.external.model.Hamster;
+import de.hamstersimulator.objectsfirst.external.model.Territory;
 import de.hamstersimulator.objectsfirst.external.simple.game.SimpleHamsterGame;
 
 /**
  * In this hamster-game {@code paule} gets four new friends.
  *
- * @author AmoresSchneyinck
- * @version 23.11.2023
+ * @author (Amores,Schneyinck)
+ * @version (23.11.23)
  */
 public class HamsterPartyGame extends SimpleHamsterGame {
+
+	/**
+	 * Creates {@code paule}s friends {@code adam}
+	 * {@code eve}
+	 * {@code erik}
+	 * {@code lupin}
+	 */
+	void createPaulesFriends() {
+
+
+		// Local variables for Adam
+		Location locationAdam = Location.from(3, 6);
+		Direction directionAdam = Direction.WEST;
+		Territory territoryAdam = game.getTerritory();
+
+		// Local variables for Eve
+		Location locationEve = Location.from(5, 3);
+		Direction directionEve = Direction.NORTH;
+		Territory territoryEve = game.getTerritory();
+
+		// Local variables for Erik
+		Location locationErik = Location.from(2,3);
+		Direction directionErik = Direction.SOUTH;
+		Territory territoryErik = game.getTerritory();
+
+		// Local variables for Lupin
+		Location locationLupin = Location.from(3, 2);
+		Direction directionLupin = Direction.EAST;
+		Territory territoryLupin = game.getTerritory();
+
+		Hamster Erik = new Hamster(territoryErik, locationErik, directionErik, 0);
+		Hamster Lupin = new Hamster(territoryLupin, locationLupin, directionLupin, 0);
+		Hamster Adam = new Hamster(territoryAdam, locationAdam, directionAdam, 0);
+		Hamster Eve = new Hamster(territoryEve, locationEve, directionEve, 0);
+
+
+
+	}
 
 	/**
 	 * Constructor for the Party Game.
@@ -17,56 +59,14 @@ public class HamsterPartyGame extends SimpleHamsterGame {
 		this.loadTerritoryFromResourceFile("/territories/territory-hamsterparty.ter");
 		this.displayInNewGameWindow();
 		game.startGame();
-	}
-
-	public class Hamster {
-		private String name;
-
-		public Hamster(String name) {
-			this.name = name;
-		}
-
-		public void display() {
-			System.out.println("Ich bin der Hamster " + name);
-		}
-
-		public void placeOnFieldWithGrain() {
-			System.out.println(name + " wurde auf ein Feld mit Korn platziert.");
-			// Hier könnten Sie weitere Logik hinzufügen, um den Hamster auf einem Feld zu positionieren.
-		}
-
-		public boolean getName() {
-		return name;
-		}
+		createPaulesFriends();
 	}
 
 	/**
 	 * Starts the HamsterPartyGame.
+	 *
 	 */
 	protected void run() {
-		// Erzeugen von Hamster-Objekten
-		Hamster adam = new Hamster("Adam");
-		Hamster eve = new Hamster("Eve");
 
-		// Platzieren der Hamster auf Feldern mit Korn
-		placeHamsterOnFieldWithGrain(adam);
-		placeHamsterOnFieldWithGrain(eve);
-
-	}
-
-	/**
-	 * places hamster on tile with corn
-	 *
-	 * @param hamster the hamster that is to be placed
-	 */
-	private void placeHamsterOnFieldWithGrain(Hamster hamster) {
-		// Hier könnten Sie die Logik hinzufügen, um den Hamster auf einem Feld mit Korn zu platzieren.
-		// Beispiel: hamster.move(); oder hamster.grab();
-		System.out.println(hamster.getName() + " has been placed on a tile with corn");
-	}
-
-	public static void main(String[] args) {
-		new HamsterPartyGame().run();
 	}
 }
-
