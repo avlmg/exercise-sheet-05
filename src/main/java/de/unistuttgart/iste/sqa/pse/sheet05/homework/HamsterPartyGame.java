26db6eb3fecae021ad41c461d809967aae393b14
yungflori <floris.schneyinck@gmail.com>
2023-11-23 15:14:19 +0100

    Update HamsterPartyGame.java

--- src/main/java/de/unistuttgart/iste/sqa/pse/sheet05/homework/HamsterPartyGame.java
+++ src/main/java/de/unistuttgart/iste/sqa/pse/sheet05/homework/HamsterPartyGame.java
@@ -5,8 +5,8 @@ import de.hamstersimulator.objectsfirst.external.simple.game.SimpleHamsterGame;
 /**
  * In this hamster-game {@code paule} gets four new friends.
  *
- * @author (Your name)
- * @version (A date or a version number)
+ * @author AmoresSchneyinck
+ * @version 23.11.2023
  */
 public class HamsterPartyGame extends SimpleHamsterGame {
 
@@ -18,12 +18,53 @@ public class HamsterPartyGame extends SimpleHamsterGame {
 		this.displayInNewGameWindow();
 		game.startGame();
 	}
-	
+
+	public class Hamster {
+		private String name;
+
+		public Hamster(String name) {
+			this.name = name;
+		}
+
+		public void display() {
+			System.out.println("Ich bin der Hamster " + name);
+		}
+
+		public void placeOnFieldWithGrain() {
+			System.out.println(name + " wurde auf ein Feld mit Korn platziert.");
+			// Hier könnten Sie weitere Logik hinzufügen, um den Hamster auf einem Feld zu positionieren.
+		}
+
+		public boolean getName() {
+		}
+	}
+
 	/**
-	 * Starts the HamsterPartyGame.	
-	 * 
+	 * Starts the HamsterPartyGame.
 	 */
 	protected void run() {
-		
+		// Erzeugen von Hamster-Objekten
+		Hamster adam = new Hamster("Adam");
+		Hamster eve = new Hamster("Eve");
+
+		// Platzieren der Hamster auf Feldern mit Korn
+		placeHamsterOnFieldWithGrain(adam);
+		placeHamsterOnFieldWithGrain(eve);
+	}
+
+	/**
+	 * Platziert den Hamster auf einem Feld mit Korn.
+	 *
+	 * @param hamster Der Hamster, der platziert werden soll.
+	 */
+	private void placeHamsterOnFieldWithGrain(Hamster hamster) {
+		// Hier könnten Sie die Logik hinzufügen, um den Hamster auf einem Feld mit Korn zu platzieren.
+		// Beispiel: hamster.move(); oder hamster.grab();
+		System.out.println(hamster.getName() + " wurde auf ein Feld mit Korn platziert.");
+	}
+
+	public static void main(String[] args) {
+		new HamsterPartyGame().run();
 	}
 }
+
